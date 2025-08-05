7938ac699689bc8abf9e146f765e4a3f3a94f37f
Niklas Palmgren <niklas.palmgren@limer.dev>
2025-08-05 21:07:43 +0200

    Corrects typo in installation script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.config/zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.config/zsh/aliases.zsh ~/.config/zsh/aliases.zsh
 ln -sf ~/dotfiles/.config/zsh/exports.zsh ~/.config/zsh/exports.zsh
-ln -sf ~/dotfiles§/.config/zsh/functions.zsh ~/.config/zsh/functions.zsh
+ln -sf ~/dotfiles/.config/zsh/functions.zsh ~/.config/zsh/functions.zsh
 
 # hypr
 mkdir -p ~/.config/hypr
