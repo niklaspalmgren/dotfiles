dabd454bd6c999c2afc5af069893e18d33277d11
Niklas Palmgren <niklas.palmgren@limer.dev>
2025-10-10 09:14:13 +0200

    Updates waybar modules

--- .config/waybar/wireguard-status.sh
+++ .config/waybar/wireguard-status.sh
@@ -3,7 +3,7 @@
 interfaces=$(wg show interfaces 2>/dev/null)
 
 if [[ -n $interfaces ]]; then
-    echo "{\"text\": \"󰦝\", \"class\": \"connected\", \"tooltip\": \"Connected to VPN $interfaces\", \"alt\": \"connected\"}"
+    echo "{\"text\": \"󰖂\", \"class\": \"connected\", \"tooltip\": \"VPN\n\nStatus: connected to $interfaces\", \"alt\": \"connected\"}"
 else
-    echo "{\"text\": \"󰦝\", \"class\": \"disconnected\", \"alt\": \"disconnected\"}"
+    echo "{\"text\": \"󰖂\", \"class\": \"disconnected\", \"tooltip\": \"VPN\n\nStatus: disconnected\", \"alt\": \"disconnected\"}"
 fi
